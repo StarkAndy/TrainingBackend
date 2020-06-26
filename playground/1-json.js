30a438110e57a77d1a4fa6b7ae322b11317d8b21
GautamB <Gautam.bo@BGMB124197.local>
2020-06-26 11:24:18 +0530

    Tes

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,30 @@
-const dataJSON={
- name:'Gyanesh',
- age:'27'
-}
+const fs = require('fs');
 
-const data=JSON.stringify(dataJSON);
+const books=[
+    {name:'Gyanesh Bohara',profession:'Software Engineer'},
+    {name:'Srini',profession:'Project Architect'}
+];
 
-const parseData=JSON.parse(data);
+const dataBuffer=JSON.stringify(books);
+fs.writeFileSync('1-json.json',dataBuffer);
 
-console.log(parseData.age);
+
+// const dataBuffer=fs.readFileSync('1-data.json');
+// const dataJson=dataBuffer.toString();
+
+// const data=JSON.parse(dataJson);
+
+// console.log(data.title);
+
+
+//checking
+// const dataJSON={
+//  name:'Gyanesh',
+//  age:'27'
+// }
+
+// const data=JSON.stringify(dataJSON);
+
+// const parseData=JSON.parse(data);
+
+// console.log(parseData.age);
